dabd216373d37be6b787e43ef4d021f450cd2de4
pipe261213 <71350172+pipe261213@users.noreply.github.com>
2024-02-21 20:07:36 -0500

    Add files via upload

---
+++ Clase 1/Nombre y promedio.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 20:56:23 2024
+
+@author: Estudiante
+"""
+
+nombre=input("Digite su nombre ")
+
+n1=int(input("Digite su primera nota: "))
+n2=int(input("Digite su segunda nota: "))
+n3=int(input("Digite su tercera nota: "))
+
+suma=(n1+n2+n3/3)
+prom= ()suma / 3)
+
+print("Su nombre es: ",nombre)
+
+print("Su promedio es: ",prom)
+print("Su primera nota es: ",n1)
+print("Su segunda nota es: ",n2)
+print("Su tercera nota es: ",n3)
---
+++ Clase 1/Suma dos numeros (1).py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  7 20:10:00 2024
+
+@author: Estudiante
+"""
+
+nombre=input("Digite nombre: ")
+
+n1 = int(input("Digite número 1: "))
+n2 = int(input("Digite número 2: "))
+suma = n1 + n2
+(print(suma))
+
+print("El numero uno es:",n1,", el segundo:",n2,", y el resultado es:",suma)
+print("Su nombre es:",nombre)
+
+#para comentarios numeral (#)#
